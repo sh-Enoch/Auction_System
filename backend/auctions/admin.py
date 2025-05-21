56fa7d98b7925fe17a8a601ccb503eaa3ea0fabb
sh-enoch <wekesawafula.enoch@gmail.com>
2025-05-22 01:37:10 +0300

    rebase merge

--- backend/auctions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Auction, Bid, CustomUser
-# Register your models here.
-
-
-admin.site.register(Auction)
-admin.site.register(Bid)
-admin.site.register(CustomUser)
